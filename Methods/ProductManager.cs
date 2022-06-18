8b60976e36098650f4d10b06b687196e51a1680a
keremraklet <kerem@raklet.com>
2022-06-18 14:08:42 +0300

    Methods project is added Variable project is removed

---
+++ Methods/ProductManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class ProductManager
+    {
+
+        public void AddProductToTheBasket(Product product)
+        {
+            // do the logic.
+            Console.WriteLine("Product is added to the basket: " + product.Name);
+        }
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.Name = "First Product";
+            product1.Price = 15.65;
+            product1.Description = "First Product's Desc.";
+
+            Product product2 = new Product();
+            product2.Id = 2;
+            product2.Name = "Second Product";   
+            product2.Price = 18.95;
+            product2.Description = "Second Product's Desc.";
+
+            Product[] prods = new Product[] {product1, product2};
+
+            foreach(Product product in prods)
+            {
+                Console.WriteLine(product.Id);
+                Console.WriteLine(product.Name);
+                Console.WriteLine(product.Price);
+                Console.WriteLine(product.Description);
+                Console.WriteLine("----------------------");
+            }
+
+            Console.WriteLine("----------METHODS------");
+            ProductManager productManager = new ProductManager();
+
+            productManager.AddProductToTheBasket(product1);
+            productManager.AddProductToTheBasket(product2);
+        }
+    }
+}
