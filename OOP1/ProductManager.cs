69ad41626b2a72763b58bb9acbc9269563036e3e
keremersann <keremersan01@gmail.com>
2022-06-19 23:00:12 -0700

    .gitignore file is added. ProductManager class is added

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    internal class ProductManager
+    {
+        public void Add(Product product)
+        {
+            product.ProductName = "Kamera";
+        }
+        public void BirSeyYap(int sayı)
+        {
+            sayı = 75;
+        }
+    }
+}
--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Product product = new Product();
+            product.Id = 1;
+            product.CategoryId = 2;
+            product.ProductName = "Masa";
+            product.UnitPrice = 500;
+            product.UnitsInStock = 3;
+
+            Product product2 = new Product { Id = 2, CategoryId = 5, ProductName = "Kalem", 
+                UnitPrice = 125.50, UnitsInStock = 250 };
+
+            //Pascal Case  // Camel Case
+            ProductManager productManager = new ProductManager();
+
+            // value type - "100" is printed
+            int sayı = 100;
+            productManager.BirSeyYap(sayı);
+            Console.WriteLine(sayı);
+
+            // Reference type - "Kamera" is printed
+            productManager.Add(product);
+            Console.WriteLine(product.ProductName);
+            
+
+
         }
     }
 }
